225393d974e423e112d7a2f47f371fb99abda8fd
Abrahman11 <60498434+Abrahman11@users.noreply.github.com>
2020-02-08 22:07:43 +0530

    Add files via upload

---
+++ Problem 2.py
@@ -0,0 +1,7 @@
+s="HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain."
+a=22
+b=27
+c=97
+d=102
+t=(s[a:b+1]+ (" ") + s[c:d+1])
+print (t)
